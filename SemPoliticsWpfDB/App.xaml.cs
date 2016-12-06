a4788b9e3c4cc1523a886b98dcf0236b68ad353d
Oleg Shatin 11-501 <mongoose0911@gmail.com>
2016-12-06 23:43:20 +0300

    init, first fix bugs with links to model

--- SemPoliticsWpfDB/App.xaml.cs
+++ SemPoliticsWpfDB/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Politics;
 
 namespace SemPoliticsWpfDB
 {
@@ -13,5 +14,18 @@ namespace SemPoliticsWpfDB
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+           using (var db = new PoliticsDBContext())
+            {
+                var agents = db.users.Where(x => x.timezone > 3);
+                foreach (users agent in agents)
+                {
+                    Console.WriteLine(agent.name);
+                }
+                Console.Read();
+            }
+
+        }
     }
 }
--- SemPoliticsWpfDB/Views/MainWindow.xaml.cs
+++ SemPoliticsWpfDB/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SemPoliticsWpfDB
+namespace SemPoliticsWpfDB.Views
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
